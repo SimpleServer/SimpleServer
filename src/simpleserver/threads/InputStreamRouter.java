c2b49d81522bfee33dace4c854f0b2aa4dc7d070
spiegalpwns <spiegalpwns@gmail.com>
2010-12-09 20:09:43 -0500

    Changed license to MIT License.

--- src/simpleserver/threads/InputStreamRouter.java
+++ src/simpleserver/threads/InputStreamRouter.java
@@ -1,19 +1,22 @@
 /*******************************************************************************
- * Copyright (C) 2010 Charles Wagner Jr..
- * spiegalpwns@gmail.com
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Open Source Initiative OSI - The MIT License:Licensing
+ * The MIT License
+ * Copyright (c) 2010 Charles Wagner Jr.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  ******************************************************************************/
 package simpleserver.threads;
 
@@ -106,9 +109,6 @@ public class InputStreamRouter implements Runnable {
 			else {
 				return line;
 			}
-		}
-		if (line.startsWith("show")) {
-			System.out.println("SimpleServer is licensed under GPL v3.\nSee the file LICENSE for more details.");
 		}
 		return line;
 	}
