71c3106fa490392f54ccb426c675cf97adef6ae3
Rusty Burchfield <GICodeWarrior@gmail.com>
2011-01-13 15:16:42 -0800

    Prep for next dev version

--- src/simpleserver/Main.java
+++ src/simpleserver/Main.java
@@ -33,7 +33,7 @@ import simpleserver.options.Options;
 public class Main {
   private static final String license = "SimpleServer -- Copyright (C) 2010 SimpleServer authors (see CONTRIBUTORS)";
   private static final String warranty = "This program is licensed under The MIT License.\nSee file LICENSE for details.";
-  private static final String baseVersion = "7.0-release";
+  private static final String baseVersion = "7.1-dev";
   private static final String version;
 
   static {
