31b373505ca7896d0cde24b990868fb55db710f6
Rusty Burchfield <GICodeWarrior@gmail.com>
2011-01-30 15:17:52 -0800

    Implement basic fallback behavior for OSX and WinXP SP2

--- src/simpleserver/Player.java
+++ src/simpleserver/Player.java
@@ -21,8 +21,10 @@
 package simpleserver;
 
 import java.io.IOException;
+import java.net.BindException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -437,8 +439,13 @@ public class Player {
 
   private static final class LocalAddressFactory {
     private static final int[] octets = { 0, 0, 1 };
+    private static Boolean canCycle = null;
 
     private synchronized String getNextAddress() {
+      if (!canCycle()) {
+        return "127.0.0.1";
+      }
+
       if (octets[2] >= 255) {
         if (octets[1] >= 255) {
           if (octets[0] >= 255) {
@@ -459,6 +466,41 @@ public class Player {
       }
 
       return "127." + octets[0] + "." + octets[1] + "." + octets[2];
+    }
+
+    private boolean canCycle() {
+      if (canCycle == null) {
+        InetAddress testDestination;
+        InetAddress testSource;
+        try {
+          testDestination = InetAddress.getByName(null);
+          testSource = InetAddress.getByName("127.0.1.2");
+        }
+        catch (UnknownHostException e) {
+          System.out.println("[SimpleServer] Notice: Unable to bind with 127.0.1.2.");
+
+          canCycle = false;
+          return false;
+        }
+
+        try {
+          Socket testSocket = new Socket(testDestination, 80, testSource, 0);
+          testSocket.close();
+        }
+        catch (BindException e) {
+          System.out.println("[SimpleServer] Notice: Unable to bind with 127.0.1.2.");
+
+          canCycle = false;
+          return false;
+        }
+        catch (IOException e) {
+          // Probably nothing listening on port 80
+        }
+
+        canCycle = true;
+      }
+
+      return canCycle;
     }
   }
 }
