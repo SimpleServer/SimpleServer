82b620fc8c2d8c4b4c634126fa8d5c5105e8b104
Anton Pirogov <anton.pirogov at googlemail.com>
2011-05-03 23:18:36 +0200

    added allowFlight to simpleserver.properties, closes #13

--- src/simpleserver/options/MinecraftOptions.java
+++ src/simpleserver/options/MinecraftOptions.java
@@ -41,6 +41,7 @@ public class MinecraftOptions extends AbstractOptions {
     options.setProperty("level-name", simpleServerOptions.get("levelName"));
     options.setProperty("spawn-animals", simpleServerOptions.get("spawnAnimals"));
     options.setProperty("spawn-monsters", simpleServerOptions.get("spawnMonsters"));
+    options.setProperty("allow-flight", simpleServerOptions.get("allowFlight"));
     options.setProperty("pvp", simpleServerOptions.get("pvp"));
 
     super.save();
