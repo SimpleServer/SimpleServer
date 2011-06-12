ed7167962f2afb704acef96386b18110cffcc6c0
Dominic Langenegger <dola@gmx.ch>
2011-06-13 00:38:30 +0200

    fixed kits with amount%64==0

--- src/simpleserver/config/KitList.java
+++ src/simpleserver/config/KitList.java
@@ -69,7 +69,9 @@ public class KitList extends PropertiesConfig {
         for (int c = 0; c < entry.amount / 64; ++c) {
           server.runCommand("give", baseCommand + " " + 64);
         }
-        server.runCommand("give", baseCommand + " " + entry.amount % 64);
+        if (entry.amount % 64 > 0) {
+          server.runCommand("give", baseCommand + " " + entry.amount % 64);
+        }
       }
       return true;
     }
