ad864235d2ae1f6765563a22ce0c9eaa119d7007
Valter Str <valters.strods@gmail.com>
2011-07-03 11:02:45 +0200

    initial /home command

---
+++ src/simpleserver/command/HomeCommand.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2010 SimpleServer authors (see CONTRIBUTORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package simpleserver.command;
+
+import simpleserver.Color;
+import simpleserver.Player;
+
+public class HomeCommand extends AbstractCommand implements PlayerCommand {
+  public HomeCommand() {
+    super("home [set|delete]", "Teleport to and manage your home");
+  }
+
+  public void execute(Player player, String message) {
+    String arguments[] = extractArguments(message);
+    if (arguments.length == 0) {
+      String waypoint = player.getServer().data.homes.getName(player.getName(true));
+      if (waypoint == null) {
+        player.addTMessage(Color.RED, "You don't have a home to teleport to!");
+        return;
+      }
+      try {
+        player.teleport(player.getServer().data.homes.get(waypoint));
+      } catch (Exception e) {
+        player.addTMessage(Color.RED, "Teleporting failed.");
+      }
+      return;
+    }
+    String command = arguments[0];
+    if (command.equals("set")) {
+      if (player.getServer().data.homes.contains(player.getName(true))) {
+        player.addTMessage(Color.RED, "You must delete your old home before saving a new one!");
+        return;
+      }
+
+      player.getServer().data.homes.set(player.getName(true), player.position);
+      player.addTMessage(Color.GRAY, "Your home has been added.");
+    } else if (command.equals("delete")) {
+      String waypoint = player.getServer().data.homes.getName(player.getName(true));
+      if (waypoint == null) {
+        player.addTMessage(Color.RED, "You don't have a home to delete!");
+        return;
+      }
+
+      player.getServer().data.homes.remove(waypoint);
+      player.addTMessage(Color.GRAY, "Your home has been deleted.");
+    } else {
+      String home = commandPrefix() + "home";
+      player.addTMessage(Color.GRAY, "Usage:");
+      player.addTMessage(Color.GRAY, "%s: teleport home", home);
+      player.addTMessage(Color.GRAY, "%s: set your home at your location", home + " set");
+      player.addTMessage(Color.GRAY, "%s: delete your home", home + " delete");
+    }
+  }
+}
---
+++ src/simpleserver/config/data/Homes.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2010 SimpleServer authors (see CONTRIBUTORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package simpleserver.config.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import simpleserver.Coordinate.Dimension;
+import simpleserver.Position;
+import simpleserver.nbt.NBTArray;
+import simpleserver.nbt.NBTByte;
+import simpleserver.nbt.NBTCompound;
+import simpleserver.nbt.NBTDouble;
+import simpleserver.nbt.NBTFloat;
+
+public class Homes {
+  private NBTCompound node;
+  private final String HOMES = "homes";
+  private final String CAPS = "capitals";
+
+  public Set<String> keys() {
+    return node.names();
+  }
+
+  public List<String> names() {
+    Set<String> keys = keys();
+    List<String> names = new ArrayList<String>(keys.size());
+    for (String key : keys) {
+      names.add(capitalize(key));
+    }
+    return names;
+  }
+
+  public boolean contains(String name) {
+    return node.containsKey(name.toLowerCase());
+  }
+
+  public String getName(String prefix) {
+    prefix = prefix.toLowerCase();
+    if (contains(prefix)) {
+      return capitalize(prefix);
+    }
+    for (String name : keys()) {
+      if (name.startsWith(prefix)) {
+        return capitalize(name);
+      }
+    }
+    return null;
+  }
+
+  private String capitalize(String name) {
+    return capitalize(name, node.getCompound(name).getArray(CAPS).get());
+  }
+
+  public Position get(String name) {
+    name = name.toLowerCase();
+    if (!node.containsKey(name)) {
+      return null;
+    }
+    NBTCompound p = node.getCompound(name);
+    double x = p.getDouble("x").get();
+    double y = p.getDouble("y").get();
+    double z = p.getDouble("z").get();
+    Dimension dim = Dimension.get(p.getByte("Dimension").get());
+    float yaw = p.getFloat("yaw").get();
+    float pitch = p.getFloat("pitch").get();
+    return new Position(x, y, z, dim, yaw, pitch);
+  }
+
+  public void set(String name, Position pos) {
+    NBTCompound p = new NBTCompound(name.toLowerCase());
+    p.put(new NBTDouble("x", pos.x));
+    p.put(new NBTDouble("y", pos.y));
+    p.put(new NBTDouble("z", pos.z));
+    p.put(new NBTByte("Dimension", pos.dimension.index()));
+    p.put(new NBTFloat("yaw", pos.yaw));
+    p.put(new NBTFloat("pitch", pos.pitch));
+    p.put(new NBTArray(CAPS, capitals(name)));
+    node.put(p);
+  }
+
+  public void remove(String name) {
+    name = name.toLowerCase();
+    node.remove(name);
+  }
+
+  void load(NBTCompound data) {
+    if (data.containsKey(HOMES)) {
+      try {
+        node = data.getCompound(HOMES);
+        capitalizeWaypoints();
+        return;
+      } catch (Exception e) {
+        System.out.println("[WARNING] Homes list is corrupt. Replacing it with empty list...");
+      }
+    }
+    node = new NBTCompound(HOMES);
+    data.put(node);
+  }
+
+  private void capitalizeWaypoints() {
+    List<String> keys = new ArrayList<String>(keys());
+    for (String name : keys) {
+      if (!node.getCompound(name).containsKey(CAPS)) {
+        node.getCompound(name).put(new NBTArray(CAPS, capitals(name)));
+        node.rename(name, name.toLowerCase());
+      }
+    }
+  }
+
+  public static byte[] capitals(String string) {
+    int length = string.length() / 8;
+    if (string.length() % 8 != 0) {
+      length++;
+    }
+    byte[] caps = new byte[length];
+    for (int i = 0; i < length; i++) {
+      byte bools = 0;
+      for (int j = 0; j < 8; j++) {
+        int pos = i * 8 + j;
+        if (pos >= string.length()) {
+          break;
+        }
+        if (string.charAt(pos) >= 0x41 && string.charAt(pos) <= 0x5a) {
+          bools = (byte) (bools | (0x1 << j));
+        }
+      }
+      caps[i] = bools;
+    }
+    return caps;
+  }
+
+  public static String capitalize(String string, byte[] capitals) {
+    StringBuilder builder = new StringBuilder(string);
+    for (int i = 0; i < string.length(); i++) {
+      if ((capitals[i / 8] & (1 << i % 8)) != 0) {
+        builder.setCharAt(i, (char) (string.charAt(i) - 32));
+      }
+    }
+    return builder.toString();
+  }
+}
