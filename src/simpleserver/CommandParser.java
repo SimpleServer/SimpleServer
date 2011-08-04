SimpleServer/SimpleServer

commit cfbce4351327f0f1a52ac2161dcbf49a0b31dad4

translation changes

src/simpleserver/CommandParser.java
@@ -142,4 +142,13 @@ public class CommandParser {
       commandClasses.put(command.getClass(), command);
     }
   }
+
+  public void reload() {
+    for (ServerCommand serverCommand : serverCommands.values()) {
+      ((AbstractCommand) serverCommand).reloadText();
+    }
+    for (PlayerCommand playerCommand : playerCommands.values()) {
+      ((AbstractCommand) playerCommand).reloadText();
+    }
+  }
 }

src/simpleserver/Server.java
@@ -42,11 +42,11 @@ import simpleserver.config.RobotList;
 import simpleserver.config.Rules;
 import simpleserver.config.Stats;
 import simpleserver.config.WhiteList;
+import simpleserver.lang.Translations;
 import simpleserver.log.AdminLog;
 import simpleserver.log.ConnectionLog;
 import simpleserver.log.ErrorLog;
 import simpleserver.minecraft.MinecraftWrapper;
-import simpleserver.options.Language;
 import simpleserver.options.Options;
 import simpleserver.rcon.RconServer;
 import simpleserver.telnet.TelnetServer;
@@ -64,8 +64,8 @@ public class Server {
   private ServerSocket socket;
   private List<String> outputLog = new LinkedList<String>();
 
-  public Language l;
   public Options options;
+  public Translations t;
   public MOTD motd;
   public KitList kits;
   public ChestList chests;
@@ -144,6 +144,10 @@ public class Server {
     robots.removeRobotPort(port);
   }
 
+  public List<Resource> getResources() {
+    return resources;
+  }
+
   public Integer[] getRobotPorts() {
     if (robots != null) {
       return robots.getRobotPorts();
@@ -177,7 +181,7 @@ public class Server {
   }
 
   public void banKickIP(String ipAddress) {
-    banKickIP(ipAddress, "Banned!");
+    banKickIP(ipAddress, t.get("Banned!"));
   }
 
   public void banKick(String name, String msg) {
@@ -192,7 +196,7 @@ public class Server {
   }
 
   public void banKick(String name) {
-    banKick(name, "Banned!");
+    banKick(name, t.get("Banned!"));
   }
 
   public void kick(String name, String reason) {
@@ -210,6 +214,13 @@ public class Server {
       playerList.updatePlayerGroups(); // reflect changes of permission.xml
       // without player relogin
     }
+
+    if (!t.setLanguage(options.get("serverLanguage"))) {
+      options.set("serverLanguage", "en");
+      options.save();
+    }
+
+    commandParser.reload();
   }
 
   public void saveResources() {
@@ -318,9 +329,9 @@ public class Server {
   }
 
   private void kickAllPlayers() {
-    String message = "Server shutting down!";
+    String message = t.get("Server shutting down!");
     if (restart) {
-      message = "Server restarting!";
+      message = t.get("Server restarting!");
     }
 
     for (Player player : playerList.getArray()) {
@@ -332,8 +343,8 @@ public class Server {
     resources = new LinkedList<Resource>();
 
     resources.add(permissions = new PermissionConfig(this));
-    resources.add(l = new Language());
     resources.add(options = new Options());
+    resources.add(t = Translations.getInstance());
     resources.add(robots = new RobotList());
     resources.add(chests = new ChestList());
     resources.add(motd = new MOTD());

src/simpleserver/command/AbstractCommand.java
@@ -21,25 +21,31 @@
 package simpleserver.command;
 
 import simpleserver.CommandParser;
+import simpleserver.lang.Translations;
 
 public abstract class AbstractCommand implements Command {
-  private final String name;
-  private final String helpText;
+  protected final String name;
+  protected final String commandCode;
+  protected String helpText;
   protected CommandParser parser;
+  protected Translations t;
 
-  protected AbstractCommand(String name, String helpText) {
+  protected AbstractCommand(String name, String commandCode) {
+    t = Translations.getInstance();
+
     if (name != null) {
-      this.helpText = name + "\u00a7f : " + helpText;
+      helpText = name + "\u00a7f : " + t.get(commandCode);
 
       int splitIndex = name.indexOf(" ");
       if (splitIndex != -1) {
         name = name.substring(0, splitIndex);
       }
     } else {
-      this.helpText = helpText;
+      helpText = t.get(commandCode);
     }
 
     this.name = name;
+    this.commandCode = commandCode;
   }
 
   public String getName() {
@@ -90,6 +96,14 @@ public abstract class AbstractCommand implements Command {
       return parser.commandPrefix();
     } else {
       return "!";
+    }
+  }
+
+  public void reloadText() {
+    if (name != null) {
+      helpText = name + "\u00a7f : " + t.get(commandCode);
+    } else {
+      helpText = t.get(commandCode);
     }
   }
 

src/simpleserver/command/InvalidCommand.java
@@ -26,16 +26,21 @@ import simpleserver.Server;
 public class InvalidCommand extends AbstractCommand implements PlayerCommand,
     ServerCommand {
   public InvalidCommand() {
-    super(null, "\u00a7cNo such command!");
+    super(null, "No such command!");
   }
 
   public void execute(Player player, String message) {
     if (!player.getServer().options.contains("alternateJarFile")) {
-      player.addMessage("\u00a7cNo such command " + message);
+      player.addMessage("\u00a7c" + t.get("No such command") + " " + message);
     }
   }
 
   public void execute(Server server, String message) {
     System.out.println("No such command " + message);
+  }
+
+  @Override
+  public void reloadText() {
+    helpText = "\u00a7c" + t.get(commandCode);
   }
 }

src/simpleserver/command/OnlinePlayerArgCommand.java
@@ -26,13 +26,13 @@ public abstract class OnlinePlayerArgCommand extends AbstractCommand implements
     PlayerCommand {
   private final boolean playerOptional;
 
-  protected OnlinePlayerArgCommand(String name, String helpText) {
-    this(name, helpText, false);
+  protected OnlinePlayerArgCommand(String name, String commandCode) {
+    this(name, commandCode, false);
   }
 
-  protected OnlinePlayerArgCommand(String name, String helpText,
+  protected OnlinePlayerArgCommand(String name, String commandCode,
                                    boolean playerOptional) {
-    super(name, helpText + " (case-insensitive, name prefix works)");
+    super(name, commandCode);
 
     this.playerOptional = playerOptional;
   }
@@ -43,7 +43,7 @@ public abstract class OnlinePlayerArgCommand extends AbstractCommand implements
     if (arguments.length > 0) {
       Player target = player.getServer().findPlayer(arguments[0]);
       if (target == null) {
-        player.addMessage("\u00a7cPlayer not online (" + arguments[0] + ")");
+        player.addMessage("\u00a7c" + t.get("Player not online") + " (" + arguments[0] + ")");
       } else {
         executeWithTarget(player, message, target);
       }
@@ -60,6 +60,17 @@ public abstract class OnlinePlayerArgCommand extends AbstractCommand implements
                                             Player target);
 
   protected void noTargetSpecified(Player player, String message) {
-    player.addMessage("\u00a7cNo player specified.");
+    player.addMessage("\u00a7c" + t.get("No player specified."));
+  }
+
+  @Override
+  public void reloadText() {
+    if (name != null) {
+      helpText = name + "\u00a7f : " + t.get(commandCode) + " "
+          + t.get("(case-insensitive, name prefix works for online players)");
+    } else {
+      helpText = t.get(commandCode) + " "
+          + t.get("(case-insensitive, name prefix works for online players)");
+    }
   }
 }

src/simpleserver/command/PlayerArgCommand.java
@@ -24,9 +24,8 @@ import simpleserver.Player;
 
 public abstract class PlayerArgCommand extends AbstractCommand implements
     PlayerCommand {
-  protected PlayerArgCommand(String name, String helpText) {
-    super(name, helpText
-        + " (case-insensitive, name prefix works for online players)");
+  protected PlayerArgCommand(String name, String commandCode) {
+    super(name, commandCode);
   }
 
   public void execute(Player player, String message) {
@@ -48,6 +47,17 @@ public abstract class PlayerArgCommand extends AbstractCommand implements
                                             String target);
 
   protected void noTargetSpecified(Player player, String message) {
-    player.addMessage("\u00a7cNo player specified.");
+    player.addMessage("\u00a7c" + t.get("No player specified."));
+  }
+
+  @Override
+  public void reloadText() {
+    if (name != null) {
+      helpText = name + "\u00a7f : " + t.get(commandCode) + " "
+          + t.get("(case-insensitive, name prefix works for online players)");
+    } else {
+      helpText = t.get(commandCode) + " "
+          + t.get("(case-insensitive, name prefix works for online players)");
+    }
   }
 }

commit 8b16295318cd85c55b04e962479de1c6532a6759

fixed /help bug introduced by cfbce4351327f0f1a52a

src/simpleserver/CommandParser.java
@@ -142,13 +142,4 @@ public class CommandParser {
       commandClasses.put(command.getClass(), command);
     }
   }
-
-  public void reload() {
-    for (ServerCommand serverCommand : serverCommands.values()) {
-      ((AbstractCommand) serverCommand).reloadText();
-    }
-    for (PlayerCommand playerCommand : playerCommands.values()) {
-      ((AbstractCommand) playerCommand).reloadText();
-    }
-  }
 }

src/simpleserver/Server.java
@@ -240,8 +240,6 @@ public class Server {
     }
 
     addressFactory.toggle(!options.getBoolean("disableAddressFactory"));
-
-    commandParser.reload();
   }
 
   public void saveResources() {

src/simpleserver/command/AbstractCommand.java
@@ -30,20 +30,18 @@ import simpleserver.Server;
 
 public abstract class AbstractCommand implements Command {
   protected final String name;
+  protected final String help;
   protected final String commandCode;
-  protected String helpText;
   protected CommandParser parser;
 
   protected AbstractCommand(String name, String commandCode) {
-    if (name != null) {
-      helpText = name + Color.WHITE + " : " + t(commandCode);
+    help = name;
 
+    if (name != null) {
       int splitIndex = name.indexOf(" ");
       if (splitIndex != -1) {
         name = name.substring(0, splitIndex);
       }
-    } else {
-      helpText = t(commandCode);
     }
 
     this.name = name;
@@ -55,10 +53,10 @@ public abstract class AbstractCommand implements Command {
   }
 
   public String getHelpText(String prefix) {
-    if (name != null) {
-      return Color.DARK_GREEN + prefix + helpText;
+    if (help != null) {
+      return Color.DARK_GREEN + prefix + help + " : " + Color.WHITE + t(commandCode);
     } else {
-      return helpText;
+      return t(commandCode);
     }
   }
 
@@ -122,14 +120,6 @@ public abstract class AbstractCommand implements Command {
       string.delete(string.length() - 2, string.length() - 1);
     }
     return string.toString();
-  }
-
-  public void reloadText() {
-    if (name != null) {
-      helpText = name + Color.WHITE + " : " + t(commandCode);
-    } else {
-      helpText = t(commandCode);
-    }
   }
 
   public void setParser(CommandParser parser) {

src/simpleserver/command/InvalidCommand.java
@@ -43,7 +43,7 @@ public class InvalidCommand extends AbstractCommand implements PlayerCommand,
   }
 
   @Override
-  public void reloadText() {
-    helpText = Color.RED + t(commandCode);
+  public String getHelpText(String prefix) {
+    return Color.RED + t(commandCode);
   }
 }

src/simpleserver/command/OnlinePlayerArgCommand.java
@@ -66,13 +66,7 @@ public abstract class OnlinePlayerArgCommand extends AbstractCommand implements
   }
 
   @Override
-  public void reloadText() {
-    if (name != null) {
-      helpText = name + Color.WHITE + " : " + t(commandCode) + " "
-          + t("(case-insensitive, name prefix works for online players)");
-    } else {
-      helpText = t(commandCode) + " "
-          + t("(case-insensitive, name prefix works for online players)");
-    }
+  public String getHelpText(String prefix) {
+    return super.getHelpText(prefix) + " " + t("(case-insensitive, name prefix works for online players)");
   }
 }

src/simpleserver/command/PlayerArgCommand.java
@@ -53,13 +53,7 @@ public abstract class PlayerArgCommand extends AbstractCommand implements
   }
 
   @Override
-  public void reloadText() {
-    if (name != null) {
-      helpText = name + Color.WHITE + " : " + t(commandCode) + " "
-          + t("(case-insensitive, name prefix works for online players)");
-    } else {
-      helpText = t(commandCode) + " "
-          + t("(case-insensitive, name prefix works for online players)");
-    }
+  public String getHelpText(String prefix) {
+    return super.getHelpText(prefix) + " " + t("(case-insensitive, name prefix works for online players)");
   }
 }