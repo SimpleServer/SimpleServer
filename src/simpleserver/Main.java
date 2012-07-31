ae489d975dac147a37cf648551aa9c0fa0e3f3aa
Simon Marti <simon@ceilingcat.ch>
2012-07-31 18:04:47 +0200

    8.4.4

--- src/simpleserver/Main.java
+++ src/simpleserver/Main.java
@@ -28,7 +28,7 @@ import java.io.InputStreamReader;
 public class Main {
   private static final String license = "SimpleServer -- Copyright (C) 2011 SimpleServer authors (see CONTRIBUTORS)";
   private static final String warranty = "This program is licensed under The MIT License.\nSee file LICENSE for details.";
-  private static final String baseVersion = "8.4.3";
+  private static final String baseVersion = "8.4.4";
   private static final boolean release = false;
   private static final String releaseState = "pre-release";
   public static final String version;
