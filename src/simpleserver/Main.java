7da37d4a48065f42df2b809e69fcb42430563aaf
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-03-12 15:46:46 -0500

    api 50 and moved version to 1.5 instead of 1.5.0

--- src/simpleserver/Main.java
+++ src/simpleserver/Main.java
@@ -33,8 +33,8 @@ public class Main {
   private static final String baseVersion = "8.4.8";
   private static final boolean release = false;
   private static final String releaseState = "release";
-  public static final int protocolVersion = 58;
-  public static final String minecraftVersion = "1.5.0";
+  public static final int protocolVersion = 60;
+  public static final String minecraftVersion = "1.5";
   public static final String version;
 
   static {
