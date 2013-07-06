cb1f25572336b681b9a0a646f6b40890706f562c
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-07-06 11:32:53 -0500

    protocol 74

--- src/simpleserver/Main.java
+++ src/simpleserver/Main.java
@@ -33,8 +33,8 @@ public class Main {
   private static final String baseVersion = "8.4.10";
   private static final boolean release = false;
   private static final String releaseState = "release";
-  public static final int protocolVersion = 73;
-  public static final String minecraftVersion = "1.6.1";
+  public static final int protocolVersion = 74;
+  public static final String minecraftVersion = "1.6.2";
   public static final String version;
 
   static {
